45fdded921c6a689b382e5eb50c412c5fb00acee
Golem <63157010+TheOriginalGolem@users.noreply.github.com>
2026-06-17 18:06:44 +0200

    init

---
+++ common/src/main/java/com/golem/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.golem;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "stackable_elytra_trims";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
